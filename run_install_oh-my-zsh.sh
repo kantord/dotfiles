a3da203845e138a2af159f58e44e0fc26055eeda
Daniel Kantor <git@daniel-kantor.com>
2023-12-19 11:30:34 +0100

    set up faster zsh status line

--- run_install_oh-my-zsh.sh
+++ run_install_oh-my-zsh.sh
@@ -4,3 +4,4 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     # Oh My Zsh is not installed, so install it
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
