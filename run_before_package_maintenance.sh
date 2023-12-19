03466cd1cf75bcf128476b3737550568725c5dd5
Daniel Kantor <git@daniel-kantor.com>
2023-12-19 16:51:00 +0100

    install lunarvim after installing os packages

--- run_before_package_maintenance.sh
+++ run_before_package_maintenance.sh
@@ -30,14 +30,22 @@ install_packages_pacman() {
   echo "Package installation completed."
 }
 
+install_lunarvim() {
+  if ! command -v nvim &> /dev/null; then
+    yes no | LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+  fi
+}
+
 # Define the file containing the list of required packages
 package_file="./required-packages.txt"
 
 # Check if apt is available
 if command -v apt &>/dev/null; then
   install_packages_apt
+  install_lunarvim
 elif command -v pacman &>/dev/null; then
   install_packages_pacman
+  install_lunarvim
 else
   echo "Error: Neither apt nor pacman is available. This script is designed for systems with apt or pacman."
   exit 1
