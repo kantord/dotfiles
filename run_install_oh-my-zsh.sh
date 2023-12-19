585ef84778310dbafbda996f158fc7787fb86644
Daniel Kantor <git@daniel-kantor.com>
2023-12-19 15:12:35 +0100

    fix oh my zsh installer

--- run_install_oh-my-zsh.sh
+++ run_install_oh-my-zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
+if [ ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
     rm -rf $HOME/.oh-my-zsh
     ZSH= sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
