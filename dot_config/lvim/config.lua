8ff61db897cc64261cced6c49f2fd0d2c92de50b
Daniel Kantor <git@daniel-kantor.com>
2023-12-28 16:52:30 +0100

    try to fix any potential problem with lvim

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -6,34 +6,34 @@
 
 lvim.plugins = {
   { 'simrat39/rust-tools.nvim' },
-  {
-    "zbirenbaum/copilot.lua",
-    event = { "VimEnter" },
-    config = function()
-      vim.defer_fn(function()
-        require("copilot").setup {
-          suggestion = { enabled = false },
-          panel = { enabled = false },
-        }
-      end, 100)
-    end,
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   event = { "VimEnter" },
+  --   config = function()
+  --     vim.defer_fn(function()
+  --       require("copilot").setup {
+  --         suggestion = { enabled = false },
+  --         panel = { enabled = false },
+  --       }
+  --     end, 100)
+  --   end,
+  -- },
   { 'sainnhe/sonokai' },
   { "mg979/vim-visual-multi" },
   { 'MattesGroeger/vim-bookmarks' },
   { 'ledger/vim-ledger' },
   { 'folke/todo-comments.nvim' },
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-neotest/neotest-python",
-      'haydenmeade/neotest-jest',
-      'rouge8/neotest-rust',
-    }
-  },
+  -- {
+  --   "nvim-neotest/neotest",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "antoinemadec/FixCursorHold.nvim",
+  --     "nvim-neotest/neotest-python",
+  --     'haydenmeade/neotest-jest',
+  --     'rouge8/neotest-rust',
+  --   }
+  -- },
   { "rcarriga/nvim-dap-ui",           dependencies = { "mfussenegger/nvim-dap" } },
   {
     "kantord/auto-save-format.nvim",
@@ -43,35 +43,35 @@ lvim.plugins = {
       }
     end,
   },
-  { 'antoinemadec/FixCursorHold.nvim' }, -- still needed: https://github.com/nvim-neotest/neotest?tab=readme-ov-file#installation
-  { "folke/neodev.nvim",              opts = {} },
+  -- { 'antoinemadec/FixCursorHold.nvim' }, -- still needed: https://github.com/nvim-neotest/neotest?tab=readme-ov-file#installation
+  -- { "folke/neodev.nvim",              opts = {} },
   { 'eugen0329/vim-esearch' },
   -- { "roverdotcom/pawfect.nvim" },
 }
 
 
 -- testing
-require("neotest").setup({
-  adapters = {
-    require("neotest-python"),
-    -- require("neotest-plenary"),
-    require('neotest-jest')({
-      jestCommand = "yarn test --watch",
-    }),
-  }
-})
+-- require("neotest").setup({
+--   adapters = {
+--     require("neotest-python"),
+--     -- require("neotest-plenary"),
+--     require('neotest-jest')({
+--       jestCommand = "yarn test --watch",
+--     }),
+--   }
+-- })
 
-lvim.keys.normal_mode["<leader>tt"] = ':lua require("neotest").run.run()<CR>'
-lvim.keys.normal_mode["<leader>td"] = ':lua require("neotest").run.run({strategy = "dap"})<CR>'
-lvim.keys.normal_mode["<leader>ta"] = ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>'
-lvim.keys.normal_mode["<leader>tA"] = ':lua require("neotest").run.run(vim.fn.getcwd())<CR>'
+-- lvim.keys.normal_mode["<leader>tt"] = ':lua require("neotest").run.run()<CR>'
+-- lvim.keys.normal_mode["<leader>td"] = ':lua require("neotest").run.run({strategy = "dap"})<CR>'
+-- lvim.keys.normal_mode["<leader>ta"] = ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>'
+-- lvim.keys.normal_mode["<leader>tA"] = ':lua require("neotest").run.run(vim.fn.getcwd())<CR>'
 
 -- colorscheme
 lvim.colorscheme = "sonokai"
 
--- make sure lsps are installed
-lvim.lsp.installer.setup.ensure_installed = { "tsserver", "pyright", "bashls", "rust_analyzer",
-  "yamlls", "marksman", "eslint" }
+-- -- make sure lsps are installed
+-- lvim.lsp.installer.setup.ensure_installed = { "tsserver", "pyright", "bashls", "rust_analyzer",
+--   "yamlls", "marksman", "eslint" }
 
 vim.g.esearch = {
   live_update = true,
