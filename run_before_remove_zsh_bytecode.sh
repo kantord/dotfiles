fe56e75e9e523f3ec65f21367abbbd9b3fbc21d8
Daniel Kantor <git@daniel-kantor.com>
2023-12-29 20:26:00 +0100

    silently remove zsh bytecode

--- run_before_remove_zsh_bytecode.sh
+++ run_before_remove_zsh_bytecode.sh
@@ -3,5 +3,5 @@
 path_to_search="$HOME/.oh-my-zsh/custom/themes/powerlevel10k/"
 
 if [ -d "$path_to_search" ]; then
-    find "$path_to_search" -type f -name '*.zwc' -exec rm -vf {} +
+    find "$path_to_search" -type f -name '*.zwc' -exec rm -vf {} + >/dev/null 2>&1
 fi
