39c4479bdc8bbb08155ab42e6d8e4ca26abc2bf0
Daniel Kantor <git@daniel-kantor.com>
2024-01-12 13:49:31 +0100

    show bluetooth headphones in status bar

--- dot_config/i3/status_command.sh
+++ dot_config/i3/status_command.sh
@@ -4,7 +4,7 @@ export I3_GNOME_POMODORO_ENV_PATH="/home/kantord/.local/bin/i3-gnome-pomodoro"
 
 while true; do
     $I3_GNOME_POMODORO_ENV_PATH status --blinkstick > /dev/null
-    s=~/repos/config/submodules/status-one-liner/status-one-liner.sh
-    echo "$($s 'mpd')│ $($s 'calendar/summary')│ $($s 'pomodoro')│ $($s 'weather:Barcelona') │ $(date +"%H:%M")"
+    s="$HOME/.config/i3/scripts/twm-status-builder/twm-status-builder.sh"
+    echo "$($s 'bluetooth/headphones')| $($s 'mpd')│ $($s 'calendar/summary')│ $($s 'pomodoro')│ $($s 'weather:Barcelona') │ $(date +"%H:%M")"
     sleep 1
 done
