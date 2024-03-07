fe5741db289436bf7859e32f283c8a691ce17e79
Daniel Kantor <git@daniel-kantor.com>
2024-03-07 10:25:23 +0100

    .

--- dot_config/lvim/executable_leovide.sh
+++ dot_config/lvim/executable_leovide.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-export LUNARVIM_RUNTIME_DIR="${LUNARVIM_RUNTIME_DIR:-"$HOME/.local/share/lunarvim"}"
-export LUNARVIM_CONFIG_DIR="${LUNARVIM_CONFIG_DIR:-"$HOME/.config/lvim"}"
-export LUNARVIM_CACHE_DIR="${LUNARVIM_CACHE_DIR:-"$HOME/.cache/lvim"}"
-export LUNARVIM_BASE_DIR="${LUNARVIM_BASE_DIR:-"$HOME/.local/share/lunarvim/lvim"}"
+export NVIM_APPNAME="${NVIM_APPNAME:-"lvim"}"
 
-exec neovide -- -u "$LUNARVIM_BASE_DIR/init.lua" "$@"
+export LUNARVIM_RUNTIME_DIR="${LUNARVIM_RUNTIME_DIR:-"/home/kantord/.local/share/lunarvim"}"
+export LUNARVIM_CONFIG_DIR="${LUNARVIM_CONFIG_DIR:-"/home/kantord/.config/lvim"}"
+export LUNARVIM_CACHE_DIR="${LUNARVIM_CACHE_DIR:-"/home/kantord/.cache/lvim"}"
+
+export LUNARVIM_BASE_DIR="${LUNARVIM_BASE_DIR:-"/home/kantord/.local/share/lunarvim/lvim"}"
+
+exec -a "$NVIM_APPNAME" neovide -- -u "$LUNARVIM_BASE_DIR/init.lua" "$@"
