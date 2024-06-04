92763fabf02c2caecf5975b58bb439020e7bdf06
Daniel Kantor <kd@stacklok.com>
2024-06-04 13:28:14 +0200

    .

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -3,8 +3,10 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local builtin = require('telescope.builtin')
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map('n', '<leader>j', builtin.find_files, { })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -36,7 +36,13 @@ return {
   },
 
   {
-    "Pocco81/auto-save.nvim"
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+      -- your config goes here
+      -- or just leave it empty :)
+    },
   },
 
   -- {
