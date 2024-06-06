e9407ef172c462b07e5281d7806d534e087b6e25
Daniel Kantor <kd@stacklok.com>
2024-06-06 12:37:15 +0200

    no format on save

--- dot_config/nvim/lua/configs/conform.lua
+++ dot_config/nvim/lua/configs/conform.lua
@@ -7,12 +7,7 @@ local options = {
     typescript = { { "prettierd", "prettier" }, {"eslint_d"} },
     javascriptreact = { { "prettierd", "prettier" }, {"eslint_d"} },
     typescriptreact = { { "prettierd", "prettier" }, {"eslint_d"} },
-  },
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  }
 }
 
 require("conform").setup(options)
