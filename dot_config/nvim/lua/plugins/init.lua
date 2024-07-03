4b8c1344876f36186e2672df5ae9fc86f6a9c49f
Daniel Kantor <git@daniel-kantor.com>
2024-07-03 08:56:09 +0200

    fix python lsp

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -20,7 +20,7 @@ return {
   	opts = {
   		ensure_installed = {
   			"lua-language-server", "stylua",
-  			"html-lsp", "css-lsp" , "prettier"
+  			"html-lsp", "css-lsp" , "prettier", "pyright"
   		},
   	},
   },
