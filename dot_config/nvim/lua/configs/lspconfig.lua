93b967a5d37934aa707060457ac0c957be399fac
Daniel Kantor <kd@stacklok.com>
2024-08-13 13:33:35 +0200

    .

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "tsserver", "eslint", 'tailwindcss', 'pyright', 'rust_analyzer' }
+local servers = { "html", "cssls", "tsserver", "eslint", 'tailwindcss', 'pyright', 'rust_analyzer', 'lua_ls' }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
