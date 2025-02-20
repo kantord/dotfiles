c81804205f4a488a232851771f0ac0b270a2f671
Daniel Kantor <git@daniel-kantor.com>
2025-02-20 13:09:50 +0100

    add neovide zoom shortcuts

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -40,6 +40,12 @@ end)
 
 require('nvim-highlight-colors').setup({})
 
+if vim.g.neovide == true then
+  vim.api.nvim_set_keymap("n", "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>", { silent = true })
+  vim.api.nvim_set_keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>", { silent = true })
+  vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
+end
+
 --
 -- require("codecompanion").setup({
 --   adapters = {
