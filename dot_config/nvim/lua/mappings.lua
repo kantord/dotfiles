66ec6b0585b5635f8295959724479c7a198e7877
Daniel Kantor <git@daniel-kantor.com>
2025-02-21 13:47:21 +0100

    fix mappings

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -3,8 +3,18 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local builtin = require('telescope.builtin')
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+
+map('n', '<leader>j', builtin.find_files, { })
+
+if vim.g.neovide == true then
+  vim.api.nvim_set_keymap("n", "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>", { silent = true })
+  vim.api.nvim_set_keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>", { silent = true })
+  vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
+end
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
