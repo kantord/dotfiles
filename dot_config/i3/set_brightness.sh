d2df5e0fe0e02963f7dca51556c7ac0ad5a98fc6
Daniel Kantor <git@daniel-kantor.com>
2025-07-05 00:01:34 +0200

    .

--- dot_config/i3/set_brightness.sh
+++ dot_config/i3/set_brightness.sh
@@ -18,7 +18,6 @@ bar=""
 for ((i = 0; i < filled; i++)); do bar+=$DOT_FILLED; done
 for ((i = 0; i < empty; i++)); do bar+=$DOT_EMPTY; done
 
-# Stack-tag: long, descriptive, i3-specific
 notify-send -a "" \
   -h "string:x-canonical-private-synchronous:i3wm.set-brightness.notification" \
   "Brightness" "$bar"
