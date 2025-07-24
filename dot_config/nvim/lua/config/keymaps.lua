8fee9cbcde42953870ebf03417ce9f7aec3a8252
Daniel Kantor <kd@stacklok.com>
2025-07-24 14:35:01 +0200

    add better keymap for finding files

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Map Space+J to find files (same as <leader>f)
+vim.keymap.set("n", "<leader>j", "<cmd>Telescope find_files<cr>", { desc = "Find Files" })
+
