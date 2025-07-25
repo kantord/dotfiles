be3d426ebc50fb425c677ddebad86e018ed9f498
Daniel Kantor <kd@stacklok.com>
2025-07-25 17:25:04 +0200

    add some themes

--- dot_config/nvim/lua/plugins/colorschemes.lua
+++ dot_config/nvim/lua/plugins/colorschemes.lua
@@ -1,5 +1,62 @@
 return {
   { "rebelot/kanagawa.nvim" },
+  { "thesimonho/kanagawa-paper.nvim" },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
+  {
+    "everviolet/nvim",
+    name = "evergarden",
+    priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+    opts = {
+      theme = {
+        variant = "fall", -- 'winter'|'fall'|'spring'|'summer'
+        accent = "green",
+      },
+      editor = {
+        transparent_background = false,
+        sign = { color = "none" },
+        float = {
+          color = "mantle",
+          solid_border = false,
+        },
+        completion = {
+          color = "surface0",
+        },
+      },
+    },
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "vague2k/vague.nvim",
+    config = function()
+      -- NOTE: you do not need to call setup if you don't want to.
+      require("vague").setup({
+        -- optional configuration here
+      })
+    end,
+  },
+  {
+    "webhooked/kanso.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  { "miikanissi/modus-themes.nvim", priority = 1000 },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    -- Add in any other configuration;
+    --   event = foo,
+    --   config = bar
+    --   end,
+  },
   {
     "sainnhe/everforest",
     lazy = false,
