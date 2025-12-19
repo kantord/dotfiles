01518930b4dada37451e7fd05d07d457a0efc550
Daniel Kantor <git@daniel-kantor.com>
2025-12-19 19:20:55 +0100

    fix

--- dot_config/i3/status_command.sh
+++ dot_config/i3/status_command.sh
@@ -5,7 +5,6 @@ export I3_GNOME_POMODORO_ENV_PATH="/home/kantord/.local/bin/i3-gnome-pomodoro"
 while true; do
     $I3_GNOME_POMODORO_ENV_PATH status --blinkstick > /dev/null
     s="$HOME/.config/i3/scripts/twm-status-builder/twm-status-builder.sh"
-    echo "$($s 'battery/headphones')| $($s 'battery/controllers')| $($s 'battery/laptop')| $($s 'mpd')│ $($s 'calendar/summary')│ $($s 'pomodoro')│ $($s 'weather:Barcelona') │ $(date +\"%H:%M\")"
+	    echo "$($s 'battery/headphones')| $($s 'battery/controllers')| $($s 'battery/laptop')| $($s 'mpd')│ $($s 'calendar/summary')│ $($s 'pomodoro')│ $($s 'weather:Barcelona') │ $(date +'%H:%M')"
     sleep 1
 done
-
